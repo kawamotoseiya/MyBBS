0fc7e406170a7fd80bd590bb2c059f69deb8ba79
kawamotoseiya <seinnto1226@icloud.com>
2020-10-19 05:33:32 +0900

    image

--- join/index.php
+++ join/index.php
@@ -1,4 +1,7 @@
 <?php
+
+session_start();
+
 if(!empty($_POST)){
 
 if($_POST['name']==''){
@@ -13,12 +16,26 @@ if($_POST['email']==''){
   if(strlen($_POST['password'])<4){
 	$error['password']='length';
   }
+  $fileName= $_FILES['image']['name'];
+  if (!empty($fileName)){
+	  $ext=substr($fileName, -3);
+	  if ($ext !='jpg' && $ext != 'gif' && $ext != 'png'){
+		  $error['image']='type';
+	  }
+  }
+
 
   if(empty($error)){
-
-  header('Location: chack.php');
-  exit();
+	$image=date('YmdHis').$_FILES['image']['name'];
+	move_uploaded_file($_FILES['image']['tmp_name'], '../member_picture/'.$image);
+	$_SESSION['join'] = $_POST;
+	$_SESSION['join']['image'] = $image;
+  	header('Location: check.php');
+  	exit();
   }
+}
+if($_REQUEST['action']=='rewrite' && isset($_SESSION['join'])){
+	$_POST = $_SESSION['join'];
 }
 ?>
 <!DOCTYPE html>
@@ -67,6 +84,12 @@ if($_POST['email']==''){
 		<dt>写真など</dt>
 		<dd>
         	<input type="file" name="image" size="35" value="test"  />
+			<?php if($error['image']==='type'): ?>
+			<p class="error">写真は。.pngや.jpgの画像をアップロードしてください。</p>
+			<?php endif; ?>
+			<?php if(!empty($error)): ?>
+				<p class="error">恐れ入りますが、画像を改めて指定してください。</p>
+			<?php endif; ?>
         </dd>
 	</dl>
 	<div><input type="submit" value="入力内容を確認する" /></div>
